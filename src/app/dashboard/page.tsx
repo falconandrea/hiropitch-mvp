6c5dbf845853676994e9e49dbbec769471521b6f
falconandrea <falcon.andrea88@gmail.com>
2024-06-16 18:44:31 +0200

    chore: remove code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,3 @@
-import { useUser } from '@clerk/nextjs';
-import { auth } from '@clerk/nextjs/server';
-
 export default async function Dashboard() {
-  const { userId } = auth();
-  const { user } = useUser();
-
-  if (!userId) {
-    return <p>You are not signed in</p>;
-  }
-
-  console.log(userId);
-  console.log(user);
-
   return <div>Dashboard</div>;
 }
