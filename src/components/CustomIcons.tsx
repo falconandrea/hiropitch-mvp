8fff84e3c24808de6737c13a688a45a425c82bc1
falconandrea <falcon.andrea88@gmail.com>
2024-06-19 20:18:25 +0200

    feat: add icons

---
+++ src/components/CustomIcons.tsx
@@ -0,0 +1,8 @@
+import { Handshake, MessageCircle, Sparkles, ThumbsUp } from 'lucide-react';
+
+export const CustomIcons = {
+  handshake: Handshake,
+  messageCircle: MessageCircle,
+  sparkles: Sparkles,
+  thumbsUp: ThumbsUp,
+};
