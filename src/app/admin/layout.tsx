470759c207d2df75cfd6d10fae71eaf3b3525a6c
falconandrea <falcon.andrea88@gmail.com>
2024-06-19 20:18:37 +0200

    wip: loading

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
 import { Sidebar } from '@/components/admin/Sidebar';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 // Vedere quali varianti usaremo
 const outfit = Outfit({
@@ -26,8 +28,10 @@ export default function Layout({
     <ClerkProvider>
       <html lang='en'>
         <body className={`${outfit.className} flex`}>
-          <Sidebar />
-          <div className='flex-1 px-8 py-16'>{children}</div>
+          <Suspense fallback={<Loading />}>
+            <Sidebar />
+            <div className='flex-1 px-8 py-16'>{children}</div>
+          </Suspense>
         </body>
       </html>
     </ClerkProvider>
---
+++ src/app/admin/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className='absolute left-0 top-0 z-10 h-full w-full bg-red-400'>
+      Loading...
+    </div>
+  );
+}
