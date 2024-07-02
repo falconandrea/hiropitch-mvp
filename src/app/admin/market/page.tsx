cb0b2418b10e8e016c095ecfcf7e58cda7bab53b
igor <igorsanna98@outlook.com>
2024-07-02 08:59:05 +0200

    Market 1.0 (da risolvere il problema del carosello che continua ad essere visualizzato solo in verticale)

--- src/app/admin/market/page.tsx
+++ src/app/admin/market/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { STATS_TABLE } from '../consts'
+import { HTMLAttributes, ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+import Button from './Button'
+import Container from './Container'
+import CollectionsSection from './CollectionsSection'
+import StatsTable from './StatsTable'
+import HeroSection from './Herosection'
+
+
 export default async function Market() {
-  return <div>Market</div>;
+  return (
+    <div>
+      <HeroSection />
+      <StatsTable />
+      <CollectionsSection title="Notable Collections" />
+      <CollectionsSection title="Top Collector Buys Today" />
+      <CollectionsSection title="LGBTQIA+ Pride Month Creator Spotlight" />
+      <CollectionsSection title="Trending in Art" />
+      <CollectionsSection title="Trending in Gaming" />
+      <CollectionsSection title="Trending in Memberships" />
+    </div>
+  );
 }
