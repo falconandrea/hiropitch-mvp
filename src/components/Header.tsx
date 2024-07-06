86ef5098b73b8c84130e48a7e39d5e756e35bb30
falconandrea <falcon.andrea88@gmail.com>
2024-07-06 13:25:14 +0200

    chore: update header removing not used links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,49 +19,11 @@ export function Header() {
       <h1 className='text-3xl font-bold text-white'>
         <Link href='/'>hiropitch</Link>
       </h1>
-      <div className='hidden items-center space-x-4 md:flex'>
-        <a href='#about' className='text-white'>
-          About
-        </a>
-        <a href='#faqs' className='text-white'>
-          FAQs
-        </a>
-        <a href='#resources' className='text-white'>
-          Resources
-        </a>
+      <div className='flex items-center space-x-4'>
         <ClerkButton
           className={`${isSignedIn ? '' : 'rounded bg-blue-500 font-bold text-white hover:bg-blue-700'}`}
         />
       </div>
-      <button className='text-white md:hidden' onClick={toggleMenu}>
-        <svg
-          className='h-6 w-6'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth={2}
-            d='M4 6h16M4 12h16m-7 6h7'
-          />
-        </svg>
-      </button>
-      {isOpen && (
-        <div className='absolute right-0 top-0 mr-4 mt-12 flex flex-col items-start space-y-4 rounded-lg bg-white p-5 shadow-lg'>
-          <a href='#about' className='hover:text-gray-700'>
-            About
-          </a>
-          <a href='#faqs' className='hover:text-gray-700'>
-            FAQs
-          </a>
-          <a href='#resources' className='hover:text-gray-700'>
-            Resources
-          </a>
-          <ClerkButton className='rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700' />
-        </div>
-      )}
     </header>
   );
 }
