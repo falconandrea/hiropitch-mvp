8172585cbdc1cd435edf0621d6777a3a885a9d58
falconandrea <falcon.andrea88@gmail.com>
2024-07-14 15:29:48 +0200

    fix: z-index loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <div className='fixed left-0 top-0 z-10 h-full w-full bg-gray-500 pt-64 text-center text-3xl opacity-80'>
+    <div className='fixed left-0 top-0 z-20 h-full w-full bg-gray-500 pt-64 text-center text-3xl opacity-80'>
       Loading...
     </div>
   );
