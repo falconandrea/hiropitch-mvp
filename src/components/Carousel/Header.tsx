c11a1ef9023d7ee1bf0dd9a7486b5632ca0bfc01
falconandrea <falcon.andrea88@gmail.com>
2024-07-14 11:35:25 +0200

    fix: scroll

--- src/components/Carousel/Header.tsx
+++ src/components/Carousel/Header.tsx
@@ -22,7 +22,9 @@ export default function Header() {
     };
   }, []);
 
-  const handleItemClick = (index: number) => {
+  const handleItemClick = (e: React.MouseEvent, index: number) => {
+    e.preventDefault();
+
     if (index === 0) {
       // Scroll to top of the page
       window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -32,9 +34,9 @@ export default function Header() {
       const element = document.getElementById(id);
       if (element) {
         const elementTop = element.getBoundingClientRect().top;
-        const offsetTop = window.pageYOffset + elementTop;
-        const currentScroll = window.pageYOffset;
-        
+        const offsetTop = window.scrollY + elementTop;
+        const currentScroll = window.scrollY;
+
         // Only scroll if the element is not already in view
         if (currentScroll !== offsetTop) {
           element.scrollIntoView({ behavior: 'smooth' });
@@ -45,20 +47,22 @@ export default function Header() {
 
   return (
     <header
-      className={`sticky top-0 z-50 border-b bg-white text-black transition-colors duration-150 ${isSticked ? 'shadow-md' : ''
-        }`}
+      className={`sticky top-0 z-50 border-b bg-white text-black transition-colors duration-150 ${
+        isSticked ? 'shadow-md' : ''
+      }`}
       ref={ref}
     >
       <Container>
         <div className='flex h-16 items-center justify-center gap-x-2 py-2.5'>
-          <ul className='gap-x-6 font-semibold flex'>
+          <ul className='flex gap-x-6 font-semibold'>
             {MAIN_TABS.map((item, index) => (
               <li
                 key={item}
-                className='hover:bg-gray-200 px-2 py-1 rounded cursor-pointer'
-                onClick={() => handleItemClick(index)}
+                className='cursor-pointer rounded px-2 py-1 hover:bg-gray-200'
               >
-                <a href='#'>{item}</a>
+                <a href='' title='' onClick={(e) => handleItemClick(e, index)}>
+                  {item}
+                </a>
               </li>
             ))}
           </ul>
